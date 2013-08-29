718e47498a86304a026432325596f6b16fd85615
Simon <kingofhawks@qq.com>
2013-08-29 14:14:03 -0700

    init commit for bitcoin project

---
+++ bitcoin/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+import views
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'bitcoin.views.home', name='home'),
+    # url(r'^bitcoin/', include('bitcoin.foo.urls')),
+    url(r'^test/$', views.test),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ bitcoin/views.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template.loader import get_template
+from django.template import Context
+from django.template import RequestContext
+from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+from django.db import transaction
+from django.db import connection
+
+# from bean import customer
+
+def test(request):
+    return render_to_response('bitcoin1.html', locals())
+

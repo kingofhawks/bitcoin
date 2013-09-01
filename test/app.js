1a103c49910b4b2a971b49b1c62e47d9e83a3e67
Simon <kingofhawks@qq.com>
2013-09-01 08:45:18 -0700

    app.js for test node server; index.html for node client;

---
+++ test/app.js
@@ -0,0 +1,26 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  //socket.emit('news', { hello: 'world' }); send to the client
+  io.sockets.emit('news', { hello: 'world' }); //send to all clients
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});

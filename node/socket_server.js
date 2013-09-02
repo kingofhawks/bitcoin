7761cbcc2bbcfd38c3f3e72f2b98879344d1cf48
Simon <kingofhawks@qq.com>
2013-09-01 20:58:59 -0700

    update socket server to integrate redis

--- node/socket_server.js
+++ node/socket_server.js
@@ -24,6 +24,14 @@ function handler (req, res) {
 }
 
 io.sockets.on('connection', function (socket) {
+	//Grab message from Redis and send to client
+    sub.on('message', function(channel, message){
+		console.log('receive redis message:'+message);
+        //socket.send(message);
+		//io.sockets.emit('news', { hello: 'world' });
+		io.sockets.emit('news', message);
+    });
+	
   //socket.emit('news', { hello: 'world' }); send to the client
   io.sockets.emit('news', { hello: 'world' }); //send to all clients
   socket.on('my other event', function (data) {

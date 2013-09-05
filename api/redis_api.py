66dbb754fe56c4a677fc51cd8e3456e85a470510
Simon <kingofhawks@qq.com>
2013-09-05 14:10:28 -0700

    support time period

--- api/redis_api.py
+++ api/redis_api.py
@@ -5,13 +5,13 @@ def publish(channel,data):
     r = redis.StrictRedis(host='192.168.192.128', port=6379, db=0)
     r.publish(channel, data)
     
-params = dict(
-        op='futures'
-    )
-
-
-
-from collector import get_ticker,get_string_data
-data = get_ticker('https://796.com/apiV2/ticker.html',params)
-#publish({'hello':'redis'})
-publish('chat',get_string_data(data))
+    
+    
+if __name__ == "__main__":     
+    params = dict(
+            op='futures'
+        )
+    
+    from collector import get_ticker,get_string_data
+    data = get_ticker('https://796.com/apiV2/ticker.html',params)
+    publish('chat',get_string_data(data))
